50dc9c253fdbcacf7b31241ac958b51e12e0a324
Mostafizur Rahman <mostafizur.rahman@surecash.net>
2020-01-20 09:27:02 +0600

    icons added

---
+++ ZigazagPuzzle/ZigazagPuzzle/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  ZigazagPuzzle
+//
+//  Created by Mostafizur Rahman on 18/1/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    
+    let dataArray = ["0" : ["icon" : ""]]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ZigazagPuzzle/ZigazagPuzzle/data/SettingTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableCell.swift
+//  ZigazagPuzzle
+//
+//  Created by Mostafizur Rahman on 18/1/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableCell: UITableViewCell {
+    
+    @IBOutlet weak var cellTitle:UILabel!
+    @IBOutlet weak var parentView: UIView!
+    @IBOutlet weak var lockImageView: UIImageView!
+    
+    
+    @IBAction func onSwitchTap(_ sender:UISwitch){
+        print("switch tap")
+    }
+    @IBAction func onButtonTap(_ sender:UIButton){
+        print("button tap")
+    }
+}
